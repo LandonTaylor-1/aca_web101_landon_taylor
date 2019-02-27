a159eef3f1abd7c94e49f16ed0457656ec340bd0
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-02-27 16:08:24 -0600

    JS

---
+++ Week 7/Tic Tac Toe/tictactoe.js
@@ -0,0 +1,59 @@
+
+var gameMarker = "X";
+
+var gameMarker = "O";
+
+function changeMarkerToX(){
+    var gameMarker = "X"
+    console.log("The X button was clicked!")
+};
+
+function changeMarkerToO(){ 
+    var gameMarker = "O"
+    console.log("The O button was clicked!")
+};
+
+function top_left(){
+    document.getElementById("top_left").innerHTML = "O";
+    console.log("The O button was clicked!")
+}
+
+function top_center(){
+    document.getElementById("top_center").innerHTML = "O";
+    console.log("The O button was clicked!")
+}
+
+function top_right(){
+    document.getElementById("top_right").innerHTML = "X";
+    console.log("The O button was clicked!")
+}
+
+function middle_left(){
+    document.getElementById("middle_left").innerHTML = "O";
+    console.log("The O button was clicked!")
+}
+
+function middle_center(){
+    document.getElementById("middle_center").innerHTML = "O";
+    console.log("The O button was clicked!")
+}
+
+function middle_right(){
+    document.getElementById("middle_right").innerHTML = "X";
+    console.log("The O button was clicked!")
+}
+
+function bottom_left(){
+    document.getElementById("bottom_left").innerHTML = "X";
+    console.log("The O button was clicked!")
+}
+
+function bottom_center(){
+    document.getElementById("bottom_center").innerHTML = "X";
+    console.log("The O button was clicked!")
+}
+
+function bottom_right(){
+    document.getElementById("bottom_right").innerHTML = "O";
+    console.log("The O button was clicked!")
+}
